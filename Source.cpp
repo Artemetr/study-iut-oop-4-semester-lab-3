2e335560c75cbf0418b15b55c40bf130180ea290
artemetr <artem@shirokikh.ru>
2020-03-22 23:43:50 +0500

    Main func

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <art-cpp.h>
+#include <iostream>
+
+using namespace art::addons;
+using namespace art::string;
+
+void main()
+{
+	List<int> list = List<int>();
+	list.append(3);
+	list.append(4);
+
+	std::cout << list << std::endl;
+	
+	
+	
+	
+	//std::cout << format("aaaasaa{1}sss", "ValXAV", "DELETE");
+}
+
+
